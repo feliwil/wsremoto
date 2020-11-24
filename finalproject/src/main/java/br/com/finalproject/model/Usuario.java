fa3b75f1fc73e38b385ebf156362f013c65aeec2
William Moraes Felicio <wfelicio@gmail.com>
2020-11-24 18:08:06 -0300

    Projeto Final -24/11

--- finalproject/src/main/java/br/com/finalproject/model/Usuario.java
+++ finalproject/src/main/java/br/com/finalproject/model/Usuario.java
@@ -18,8 +18,8 @@ public class Usuario {
 	@Column(name="nome", length = 100)
 	private String nome;
 	
-	@Column (name="email", length = 100)
-	private String email;
+	@Column (name="racf", length = 10)
+	private String racf;
 	
 	@Column(name="senha",length = 20)
 	private String senha;
@@ -30,51 +30,6 @@ public class Usuario {
 	
 	
 	
-	public Usuario() {
-		super();
-	}
-	
-	public Usuario(int id, String nome, String email, String senha, String foto) {
-		super();
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.senha = senha;
-		this.foto = foto;
-	}
-	
-
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	public String getFoto() {
-		return foto;
-	}
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
 	
 	
 }
